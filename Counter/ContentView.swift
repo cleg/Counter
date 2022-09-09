5f333add5e2e06ebf30ec7a4b173d59eaa726682
Paul Dmitryev <clegger@gmail.com>
2022-09-09 11:02:22 +0300

    Initial Commit

---
+++ Counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Counter
+//
+//  Created by Paul Dmitryev on 09.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
