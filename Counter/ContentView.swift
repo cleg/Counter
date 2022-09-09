32ca61569ffeaafd298004569484e175f7fd4926
Paul Dmitryev <clegger@gmail.com>
2022-09-09 11:52:07 +0300

    feature: basic skeleton

--- Counter/ContentView.swift
+++ Counter/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+    @StateObject private var viewModel = ViewModel()
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    var body: some View {
+        VStack(spacing: 32.0) {
+            Text("\(viewModel.counter)")
+
+            Button("Increase counter") {
+                viewModel.increaseCounter()
+            }
+
+            Button("Counter details") {
+                viewModel.showDetails()
+            }
+
+            Button("Show date") {
+                viewModel.showDate()
+            }
+        }
+        .fullScreenCover(item: $viewModel.cover) { cover in
+            /// NavigationLink would be more logical in terms of UX, but `NavigationLink` by
+            /// default doesn't have version with optional binding, and I didn't want to bring it
+            /// here to keep example as small as possible, so full screen covers will be OK too
+            ///
+            /// Part below can be improved by introducing some form of Coordinator pattern
+            /// that will take care about navigation, and, probably, a view builder, but
+            /// let's keep focused on the simplest possible way of doing things
+            switch cover {
+            case let .currentValue(counterValue, date):
+                CounterValueView(
+                    currentValue: counterValue,
+                    lastChangDate: date
+                ) { reset in
+                    if reset {
+                        viewModel.resetCounter()
+                    }
+                    viewModel.dismissCover()
+                }
+            case let .showDate(date):
+                DateView(dateChanged: date) {
+                    viewModel.dismissCover()
+                }
+            }
+        }
     }
 }
